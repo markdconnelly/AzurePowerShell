ae7c71507dbd3f4bd319c1a576bdf03094bdd53a
Mark.Connelly <Mark.Connelly@carle.com>
2023-01-20 11:20:42 -0600

    Shifted Notes from Code to Excel

--- 03-Collect Enterprise App Facts/Collect Azure Enterprise App Facts.ps1
+++ 03-Collect Enterprise App Facts/Collect Azure Enterprise App Facts.ps1
@@ -23,11 +23,13 @@
     $arrAppRegSecretRaw = @()
     $arrAppRegCertificateRaw = @()
     $arrAppWebRaw = @()
-    $strDirObjFacts = ""
+    $strEntAppDirObjFacts = ""
+    $strAppRegDirObjFacts = ""
     $strAppFacts = ""
     $strSrvPrcFacts = ""
     $strAppDisplayName = ""
-    $strAppObjectID = ""
+    $strEntAppObjectID = ""
+    $strAppRegObjectID = ""
     $strAppID = ""
     $strAppMembershipRaw = ""
     $strAppOwnerRaw = ""
@@ -39,7 +41,7 @@
     #################################################### <Enterprise Application Certificate Variables> ########################################
     #region Enterprise Application Certificate Variables
     $psobjEntAppCertOutput = @()
-    $arrAppCertificateRaw = @()
+    $arrEntAppCertificateRaw = @()
     $arrAppRegCertificateRaw = @()
     #endregion Enterprise Application Certificate Variables
     #################################################### </Enterprise Application Certificate Variables> #######################################
@@ -70,7 +72,7 @@
 #   An API connection to graph is assumed at this point-if you have not connected, please see the "Connect-Graph" folder in this repo
     #################################################### <Build Enterprise App Table> ############################################################
     #region Build Enterprise App Table 
-        
+
         #  To start, we fill an array with the full output of all service principals
         $arrAAD_Applications = Get-MgServicePrincipal -All:$true | Where-Object {$_.Tags -eq "WindowsAzureActiveDirectoryIntegratedApp"}
 
@@ -78,15 +80,22 @@
         foreach ($app in $arrAAD_Applications) {
             #   The general command Get-MgServicePrincipal primarily has these 3 core facts that we can pivot on for specific sub-commands
             $strAppDisplayName = $app.DisplayName 
-            $strAppObjectID = $app.Id
+            $strEntAppObjectID = $app.Id
             $strAppID = $app.AppId
 
             #   Enterprise applications are Applications, Directory Objects, and Service Principals. Because of this, we collect 3 arrays for 
-            #   each object type per application. 
-            $strDirObjFacts =  Get-MgDirectoryObject -DirectoryObjectId $strAppObjectID 
+            #   each object type per application. We also do the AppFacts first because App Registrations have unique object IDs that we need
+            #   to extract before getting the Directory Object
             $strAppFacts = Get-MgApplication -Filter "AppId eq '$strAppID'"
-            $strSrvPrcFacts = Get-MgServicePrincipal -Filter "Id eq '$strAppObjectID'"
-            $strDirObjFacts = $strDirObjFacts.AdditionalProperties
+            $strAppRegObjectID = $strAppFacts.Id
+            $strSrvPrcFacts = Get-MgServicePrincipal -Filter "Id eq '$strEntAppObjectID'"
+            $strEntAppDirObjFacts = Get-MgDirectoryObject -DirectoryObjectId $strEntAppObjectID
+            $strAppRegDirObjFacts = Get-MgDirectoryObject -DirectoryObjectId $strAppRegObjectID
+            $strEntAppDirObjFacts = $strEntAppDirObjFacts.AdditionalProperties
+            $strAppRegDirObjFacts = $strAppRegDirObjFacts.AdditionalProperties
+
+$Fix extraction strings after this
+
 
             #   Set attributes that don't require further parsing
             $strPreferredSSOMode = $strDirObjFacts.PreferredSingleSignOnMode
@@ -112,26 +121,18 @@
             $strAppMemberOfRaw = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $strAppObjectID
             $strAppMemberOfParsed = $strAppMemberOfRaw.ResourceDisplayName | Out-String
 
-            #extract and parse the Oauth Permissions
-
-
-            #   extract and parse the sub objects of the Certificate elements
-            $arrAppCertificateRaw = $strSrvPrcFacts.KeyCredentials
-            $arrAppRegCertificateRaw = $strAppFacts.KeyCredentials
-            $arrThisAppCertificates = @()
-
-
-
             #   extract and parse the sub objects of the Secret element
             $arrAppRegSecretRaw = $strAppFacts.PasswordCredentials
             $arrThisAppSecrets = @()
-
-
+            $psobjThisAppSecrets = @()
             foreach($secret in $arrAppRegSecretRaw){
+                #   set loop variables that need to be reset for each iteration to calculate days to expiration
                 $loopVarEndDate = ""
                 $loopVarDaysToExpire = ""
                 $loopVarEndDate = $secret.EndDateTime
                 $loopVarDaysToExpire = New-TimeSpan -Start $dateNow -End $loopVarEndDate
+
+                #   PSObj to store the full secret table
                 $psobjEntAppSecretOutput += [PSCustomObject]@{
                     KeyName = $secret.DisplayName 
                     SecretID = $secret.KeyId
@@ -140,9 +141,191 @@
                     DaysToExpire = $loopVarDaysToExpire.Days
                     AppDisplayName = $strAppDisplayName
                     AppObjectID = $strAppObjectID
+                } ##[PSCustomObject]@{}  
+
+                #   PSObj to store a table of this specific apps secret ID and name
+                $psobjThisAppSecrets += [PSCustomObject]@{
+                    KeyName = $secret.DisplayName 
+                    SecretID = $secret.KeyId
+                } ##[PSCustomObject]@{} 
+            }
+            $arrThisAppSecrets = $psobjThisAppSecrets | Out-String
+
+            #   extract and parse the sub objects of the Certificate elements
+            $arrEntAppCertificateRaw = $strSrvPrcFacts.KeyCredentials
+            $arrAppRegCertificateRaw = $strAppFacts.KeyCredentials
+            $arrThisAppCertificates = @()
+
+
+
+
+            foreach($cert in $arrAppRegCertificateRaw){
+                $loopVarEndDate = ""
+                $loopVarDaysToExpire = ""
+                $loopVarEndDate = $cert.EndDateTime
+                $loopVarDaysToExpire = New-TimeSpan -Start $dateNow -End $loopVarEndDate
+                $psobjEntAppCertOutput += [PSCustomObject]@{
+                    CertName = $cert.DisplayName
+                    CertID = $cert.KeyId
+                    StartDate = $cert.StartDateTime
+                    EndDate = $cert.EndDateTime
+                    DaysToExpire = $loopVarDaysToExpire.Days
+                    Type = "App Registration"
+                    Usage = $cert.Usage
+                    Thumbprint = $cert.CustomKeyIdentifier
+                    AppDisplayName = $strAppDisplayName
+                    AppObjectID = $strAppObjectID
                 } ##[PSCustomObject]@{}   
             }
-            $arrThisAppSecrets = $psobjEntAppSecretOutput | fl DisplayName, SecretID | Out-String
+            foreach($cert in $arrAppCertificateRaw){
+                $loopVarEndDate = ""
+                $loopVarDaysToExpire = ""
+                $loopVarEndDate = $cert.EndDateTime
+                $loopVarDaysToExpire = New-TimeSpan -Start $dateNow -End $loopVarEndDate
+                $psobjEntAppCertOutput += [PSCustomObject]@{
+                    CertName = $cert.DisplayName
+                    CertID = $cert.KeyId
+                    StartDate = $cert.StartDateTime
+                    EndDate = $cert.EndDateTime
+                    DaysToExpire = $loopVarDaysToExpire.Days
+                    Type = "Enterprise Application"
+                    Usage = $cert.Usage
+                    Thumbprint = $cert.CustomKeyIdentifier
+                    AppDisplayName = $strAppDisplayName
+                    AppObjectID = $strAppObjectID
+                } ##[PSCustomObject]@{}  
+            }
+            $arrThisAppCertificates = $psobjEntAppCertOutput.CertID | Out-String
+
+
+
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #   extract and parse the sub objects of the Certificate elements
+            $arrAppCertificateRaw = $strSrvPrcFacts.KeyCredentials
+            $arrAppRegCertificateRaw = $strAppFacts.KeyCredentials
+            $arrThisAppCertificates = @()
+
 
 
 
@@ -199,7 +382,7 @@
                 Identifier = $strIdentifierURI
                 SignInURL = $strSignInURL
                 LogoutURL = $strLogoutURL
-                Secrets = $arrThisAppSecretIDs
+                Secrets = $arrThisAppSecrets
                 Certificates = $arrThisAppCertificateIDs
             } ##[PSCustomObject]@{}
         }
@@ -217,212 +400,3 @@
 
 #################################################### </Functions> ##############################################################################
 ################################################################################################################################################
-
-<#
-Attributes:
-    DirObjEntApp Facts: (Get-MgDirectoryObject $AppRegObjectID)
-        - appId
-        - displayName
-        - identifierUris
-        - publisherDomain
-        - signInAudience
-        - api
-            - knownClientApplications
-            - preAuthorizedApplications
-            - oauth2PermissionScopes
-                - adminConsentDescription
-                - adminConsentDisplayName
-                - id
-                - isEnabled
-                - type
-                - userConsentDescription
-                - userConsentDisplayName
-                - value
-        - appRoles
-            - allowedMemberTypes
-            - description
-            - displayName
-            - id
-            - isEnabled
-            - origin (example value was Application )
-        - keyCredentials (Contains app certificates only)
-            - displayName
-            - endDateTime
-            - keyId
-            - startDateTime
-            - type (Example = AsymmetricX509Cert)
-            - usage
-        - passwordCredentials (Contains app secrets)
-            - displayName
-            - endDateTime
-            - keyId
-            - startDateTime
-        - publicClient
-            - redirectUris
-        - requiredResourceAccess
-            - resourceAppId
-            - resourceAccess
-                - id
-                - type
-        - web
-            - homePageUrl
-            - logoutUrl
-            - redirectUris
-            - implicitGrantSettings
-                - enableAccessTokenIssuance
-                - enableIdTokenIssuance
-            - redirectUriSettings
-    DirObjAppReg Facts: (Get-MgDirectoryObject $EntAppObjectID)
-        - appDisplayName
-        - appId
-        - appOwnerOrganizationId
-        - displayName
-        - homepage
-        - logoutUrl
-        - notificationEmailAddresses
-        - preferredSingleSignOnMode
-        - preferredTokenSigningKeyThumbprint
-        - replyUrls
-        - servicePrincipalType
-        - signInAudience
-        - appRoles
-            - allowedMemberTypes
-            - description
-            - displayName
-            - id
-            - isEnabled
-            - origin
-        - keyCredentials
-            - displayName
-            - endDateTime
-            - keyId
-            - startDateTime
-            - type
-            - usage
-        - oauth2PermissionScopes
-            - adminConsentDescription
-            - adminConsentDisplayName
-            - id
-            - isEnabled
-            - type
-            - userConsentDescription
-            - userConsentDisplayName
-            - value
-    AppReg Facts: (Get-MgApplication $ApplicationID)
-        - Api
-         - AcceptMappedClaims
-         - KnownClientApplications
-         - Oauth2PermissionScopes
-            - AdminConsentDescription
-            - AdminConsentDisplayName
-            - Id
-            - IsEnabled
-            - Type
-            - UserConsentDescription
-            - UserConsentDisplayName
-            - Value
-        - AppId
-        - AppRoles
-            - AllowedMemberTypes
-            - Description
-            - DisplayName
-            - Id
-            - IsEnabled
-            - Origin
-            - Value
-        - DisplayName
-        - GroupMembershipClaims
-        - Id
-        - IdentifierUris
-        - keyCredentials (Contains app certificates only)
-            - displayName
-            - endDateTime
-            - keyId
-            - startDateTime
-            - type (Example = AsymmetricX509Cert)
-            - usage
-        - OptionalClaims
-            - AccessToken
-            - IdToken
-            - Saml2Token
-        - Owners
-        - passwordCredentials (Contains app secrets)
-            - displayName
-            - endDateTime
-            - keyId
-            - startDateTime
-        - PublicClient
-            - RedirectUris
-        - requiredResourceAccess
-            - resourceAppId
-            - resourceAccess
-                - id
-                - type
-        - SamlMetadataUrl
-        - SignInAudience
-        - TokenEncryptionKeyId
-        - TokenIssuancePolicies
-        - Web
-            - HomePageUrl
-            - ImplicitGrantSettings
-                - EnableAccessTokenIssuance
-                - EnableIdTokenIssuance
-            - LogoutUrl
-            - RedirectUris
-    EntApp Facts: (Get-MgServicePrincipal $EntAppObjectID)
-        - AppDisplayName
-        - AppId
-        - AppOwnerOrganizationId
-        - AppRoleAssignedTo
-        - AppRoleAssignmentRequired
-        - AppRoleAssignments
-        - AppRoles
-            - AllowedMemberTypes
-            - Description
-            - DisplayName
-            - Id
-            - IsEnabled
-            - Origin
-            - Value
-        - ClaimsMappingPolicies
-        - CreatedObjects
-        - DelegatedPermissionClassifications
-        - DisplayName
-        - Endpoints
-        - FederatedIdentityCredentials
-        - Id
-        - keyCredentials (Contains sso certificates only)
-            - displayName
-            - endDateTime
-            - keyId
-            - startDateTime
-            - type (Example = AsymmetricX509Cert)
-            - usage
-        - LoginUrl
-        - LogoutUrl
-        - MemberOf
-        - NotificationEmailAddresses
-        - Oauth2PermissionGrants
-        - Oauth2PermissionScopes
-            - AdminConsentDescription
-            - AdminConsentDisplayName
-            - Id
-            - IsEnabled
-            - Type
-            - UserConsentDescription
-            - UserConsentDisplayName
-            - Value
-        - OwnedObjects
-        - Owners
-        - PreferredSingleSignOnMode
-        - PreferredTokenSigningKeyThumbprint
-        - ReplyUrls
-        - SamlSingleSignOnSettings
-            - RelayState
-        - ServicePrincipalNames
-        - ServicePrincipalType
-        - SignInAudience
-        - TokenEncryptionKeyId
-        - TokenIssuancePolicies
-        - TokenLifetimePolicies
-#>
