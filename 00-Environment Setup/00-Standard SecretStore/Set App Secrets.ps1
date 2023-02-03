a414a3ffdb30782acec24d31c63bf4a3234c131d
Mark Connelly <mark.d.connelly@outlook.com>
2023-02-02 21:24:25 -0600

    update

--- 00-Environment Setup/00-Standard SecretStore/Set App Secrets.ps1
+++ 00-Environment Setup/00-Standard SecretStore/Set App Secrets.ps1
@@ -2,10 +2,13 @@ Install-Module Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.Secre
 Register-SecretVault -Name LocalSecretStore -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
 Set-Secret -Name PSAppID -Secret "(Your Application (Client) ID Here)"
 Set-Secret -Name PSAppTenantID -Secret "(Your Tenant ID Here)"
+Set-Secret -Name PSAppOrganizationName -Secret "(Your Organization Domain Name Here)"
 Set-Secret -Name PSAppSecret -Secret "(Your Client Secret Here)"
+Set-Secret -Name PSAppCert -Secret "(Your Certificate Here)"
 Set-Secret -Name PSAppDisplayName -Secret "(Your Application Display Name Here)"
 Set-Secret -Name PSEntAppObjectID -Secret "(Your Application Object ID Here)"
 Set-Secret -Name PSMSIDisplayName -Secret "(Your Managed Identity Display Name Here)"
+Set-Secret -Name PSMSIObjectID -Secret "(Your Managed Identity Object ID Here)"
 Set-Secret -Name ITGlueAPI_URL -Secret "(Your ITGlue API URL Here)"
 Set-Secret -Name ITGlueAPI_Key -Secret "(Your ITGlue API Key Here)"
 
